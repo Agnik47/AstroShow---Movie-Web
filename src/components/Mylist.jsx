fbe43448b4cbafd276418164df49d75a033b4542
Agnik47 <140933190+Agnik47@users.noreply.github.com>
2024-10-24 00:02:54 +0530

    Small Changes add a Movie Rating

--- src/components/Mylist.jsx
+++ src/components/Mylist.jsx
@@ -7,7 +7,7 @@ const Mylist = () => {
   return (
     <div className="text-center  flex justify-center items-center text-white ml-[29%]">
       <h1 className="text-6xl"> I will make it Verry Soon.....</h1> <br />
-      <button onClick={()=>navigate(-1)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      <button onClick={()=>navigate(-1)} className=" bg-[#6556CD] hover:bg-[#5043A9] text-xl text-white font-bold py-2 px-4 rounded-full">
         Back
       </button>
     </div>
--- src/partials/Cards.jsx
+++ src/partials/Cards.jsx
@@ -8,6 +8,9 @@ const Cards = ({ data, title }) => {
     <div className="grid mt-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 bg-[#1F1E24] px-[2vw]">
       {data.map((card, index) => {
         // Determine the image path from the available options
+        {
+          console.log(card);
+        }
         const imageUrl =
           card.backdrop_path || card.poster_path || card.profile_path;
 
@@ -27,6 +30,13 @@ const Cards = ({ data, title }) => {
               }
               className="w-full h-[400px] object-cover"
             />
+            
+            {/* Raiting Div */}
+            <div className="Rating absolute w-[3.5vw] h-[3.5vw] rounded-full flex justify-center items-center right-3 top-3 bg-gradient-to-r from-[#6556CD] to-pink-500 text-white shadow-lg text-sm font-semibold transform -translate-y-1/2 translate-x-1/2">
+              {(card.vote_average * 10).toFixed()} 
+              <span className="text-xs">%</span>
+            </div>
+
             <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-75 group-hover:opacity-100 transition-opacity duration-300"></div>
             <div className="absolute bottom-0 left-0 p-4">
               <h3 className="text-white text-lg font-bold">
