04d24a0cd3febc62c2439bd7cbfbf0989475252f
Agnik47 <140933190+Agnik47@users.noreply.github.com>
2024-10-28 22:31:23 +0530

    Add all changes to main repo

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,25 +1,33 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { asyncLoadMovie, removeMovie } from '../store/actions/movieAction';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { asyncLoadMovie, removeMovie } from "../store/actions/movieAction";
+import { useParams } from "react-router-dom";
 
-const   MovieDetails = () => {
+const MovieDetails = () => {
   const dispatch = useDispatch();
-  const {id} = useParams();
-
+  const { id } = useParams();
+  const { info } = useSelector((state) => state.movie);
+  console.log(info);
   useEffect(() => {
     dispatch(asyncLoadMovie(id));
     return () => {
       dispatch(removeMovie());
-    }
-  },[]);
+    };
+  }, []);
 
+  return info && info.detail ? (
+    <div
+      style={{
+        background: `linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.7),rgba(0,0,0,.9)), url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="text-white w-screen h-screen"
+    ></div>
+  ) : (
+    <h1>Loading...</h1>
+  );
+};
 
-  return (
-    <div className='text-white flex  items-center justify-center h-screen'>
-        <h1 className='text-9xl font-bold'>Movie Details</h1>
-    </div>
-  ) 
-}
-
-export default MovieDetails 
+export default MovieDetails;
